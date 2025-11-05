b91bd73ae9247ccbb95a093fc5390d0d753dc89d
H-R-Wells2 <hrwells10246@gmail.com>
2025-11-05 21:11:31 +0530

    trying to fix production login

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-//   baseURL: process.env.NEXT_PUBLIC_APP_URL,
+  baseURL: "https://hrleaf.vercel.app",
 });
 
 export const { signIn, signUp, useSession, signOut } = createAuthClient();
