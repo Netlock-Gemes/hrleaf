3d30aa9030f50ffd8e5e9205df9442454877b944
H-R-Wells2 <hrwells10246@gmail.com>
2025-11-05 20:48:42 +0530

    updated env import

--- lib/auth.ts
+++ lib/auth.ts
@@ -9,9 +9,8 @@ export const auth = betterAuth({
   }),
   socialProviders: {
         google: {
-            prompt: "select_account",
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
         }
     },
 });
